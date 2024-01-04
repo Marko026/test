bc521ed17fc73877322af50bd4f2031f7272d931
Marko026 <Markec026@yahoo.com>
2024-01-04 18:56:56 +0100

    Update Filter component and package dependencies

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -11,7 +11,7 @@ import CheckBoxes from "../Search/CheckBoxes";
 
 const Filter = ({ searchParams }: SearchParamProps) => {
   const [maxPrice, setMaxPrice] = useState(0);
-  let searchParamsInstance = new URLSearchParams(searchParams);
+  let searchParamsInstance = new URLSearchParams(JSON.stringify(searchParams));
 
   const router = useRouter();
 
@@ -24,71 +24,151 @@ const Filter = ({ searchParams }: SearchParamProps) => {
   }, [maxPrice]);
 
   const handleChange = (key: string, value: string | number) => {
-    let values = searchParamsInstance.get(key) ? searchParamsInstance.get(key).split(",") : [];
+    let values = searchParamsInstance.get(key)
+      ? searchParamsInstance.get(key)?.split(",")
+      : [];
 
     const valueAsString = String(value);
 
-    if (key === "rentPrice" && !values.includes(valueAsString)) {
+    if (key === "rentPrice" && values && !values.includes(valueAsString)) {
       values = [valueAsString];
-    } else if (values.includes(valueAsString)) {
+    } else if (values?.includes(valueAsString)) {
       values = values.filter((t) => t !== valueAsString);
     } else {
+      values = values ?? [];
       values.push(valueAsString);
     }
 
     searchParamsInstance.set(key, values.join(","));
 
-    searchParams = searchParamsInstance.toString();
+    searchParams = Object.fromEntries(Array.from(searchParamsInstance));
 
     const newUrl = formUrlQuery({
-      params: searchParams,
+      params: JSON.stringify(searchParams),
       key: key,
-      value: new URLSearchParams(searchParams).get(key),
+      value: new URLSearchParams(JSON.stringify(searchParams)).get(key),
     });
 
     router.push(newUrl, { scroll: false });
   };
 
   return (
-    <div className="lg:pt-[41px] lg:px-6 lg:bg-white-50 dark:bg-gray-900 lg:dark:border-r-[1px] border-gray-850 ">
-      <div className="hidden lg:block text-xs font-semibold text-blue-100 mb-7">SEARCH</div>
+    <>
+      <div className="lg:pt-[41px] lg:px-6 lg:bg-white-50 dark:bg-gray-900 lg:dark:border-r-[1px] border-gray-850 ">
+        <div className="hidden lg:block text-xs font-semibold text-blue-100 mb-7">
+          SEARCH
+        </div>
 
-      <div className="flex gap-4 mb-8 lg:mb-[52px] ">
-        <input type="text" placeholder="Search something here" className="w-full h-[46px] dark:border-gray-800 dark:bg-gray-850 text-gray-700 font-normal text-xs leading-6 px-[11px] py-4 bg-white-50 rounded-md border border-blue-50" />
-        <div className="h-[48px] w-[48px] bg-white-50 rounded-[10px] flex items-center justify-center p-3 border border-blue-50 dark:bg-gray-900 dark:border-gray-800">
-          <Image src="/icons/filter-dark.png" width={24} height={24} alt="filter" />
+        <div className="flex gap-4 mb-8 lg:mb-[52px] ">
+          <input
+            type="text"
+            placeholder="Search something here"
+            className="w-full h-[46px] dark:border-gray-800 dark:bg-gray-850 text-gray-700 font-normal text-xs leading-6 px-[11px] py-4 bg-white-50 rounded-md border border-blue-50"
+          />
+          <div className="h-[48px] w-[48px] bg-white-50 rounded-[10px] flex items-center justify-center p-3 border border-blue-50 dark:bg-gray-900 dark:border-gray-800">
+            <Image
+              src="/icons/filter-dark.png"
+              width={24}
+              height={24}
+              alt="filter"
+            />
+          </div>
+        </div>
+
+        <div className="hidden lg:block ">
+          <div className="hidden lg:block text-xs  font-semibold text-blue-100 mb-7">
+            TYPE
+          </div>
+
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"type"}
+            capacity={""}
+            type={"Sports"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"type"}
+            capacity={""}
+            type={"Hybrid"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"type"}
+            capacity={""}
+            type={"Electric"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"type"}
+            capacity={""}
+            type={"Sedan"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"type"}
+            capacity={""}
+            type={"Coupe"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"type"}
+            capacity={""}
+            type={"Hatchback"}
+          />
+
+          <div className="mt-14"></div>
+
+          <div className="hidden lg:block text-xs  font-semibold text-blue-100 mb-7">
+            CAPACITY
+          </div>
+
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"capacity"}
+            type={"someType"}
+            capacity={"2"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"capacity"}
+            type={"someType"}
+            capacity={"4"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"capacity"}
+            type={"someType"}
+            capacity={"5"}
+          />
+          <CheckBoxes
+            handleChange={handleChange}
+            category={"capacity"}
+            type={"someType"}
+            capacity={"8"}
+          />
+
+          <div className="mt-14"></div>
+
+          <div className="hidden lg:block text-xs  font-semibold text-blue-100 mb-7">
+            PRICE
+          </div>
+
+          <div>
+            <Slider
+              className="w-[246px]"
+              name="slider"
+              defaultValue={[0]}
+              max={200}
+              onValueChange={(value: number[]) => onValueChange(value)}
+            />
+            <div className="mt-3 text-xl font-semibold text-gray-700">
+              Max. ${maxPrice.toFixed(2)}
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="hidden lg:block ">
-        <div className="hidden lg:block text-xs  font-semibold text-blue-100 mb-7">TYPE</div>
-
-        <CheckBoxes handleChange={handleChange} category={"type"} type={"Sports"} />
-        <CheckBoxes handleChange={handleChange} category={"type"} type={"Hybrid"} />
-        <CheckBoxes handleChange={handleChange} category={"type"} type={"Electric"} />
-        <CheckBoxes handleChange={handleChange} category={"type"} type={"Sedan"} />
-        <CheckBoxes handleChange={handleChange} category={"type"} type={"Coupe"} />
-        <CheckBoxes handleChange={handleChange} category={"type"} type={"Hatchback"} />
-
-        <div className="mt-14"></div>
-
-        <div className="hidden lg:block text-xs  font-semibold text-blue-100 mb-7">CAPACITY</div>
-
-        <CheckBoxes handleChange={handleChange} category={"capacity"} capacity={"2"} />
-        <CheckBoxes handleChange={handleChange} category={"capacity"} capacity={"4"} />
-        <CheckBoxes handleChange={handleChange} category={"capacity"} capacity={"5"} />
-        <CheckBoxes handleChange={handleChange} category={"capacity"} capacity={"8"} />
-
-        <div className="mt-14"></div>
-
-        <div className="hidden lg:block text-xs  font-semibold text-blue-100 mb-7">PRICE</div>
-
-        <div>
-          <Slider className="w-[246px]" name="slider" defaultValue={[0]} max={200} onValueChange={(value: number[]) => onValueChange(value)} />
-          <div className="mt-3 text-xl font-semibold text-gray-700">Max. ${maxPrice.toFixed(2)}</div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
